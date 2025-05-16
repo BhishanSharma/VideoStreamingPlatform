3d3a9556d5231cd4b90f555c75dc2fb26b4065d6
BhishanSharma <bhishansharma2@gmail.com>
2025-05-16 13:32:58 +0530

    update like routes

--- src/routes/like.route.js
+++ src/routes/like.route.js
@@ -5,30 +5,30 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 // Like a comment
-router.post("/like/comment/:commentId", verifyJWT, createLike);
+router.post("/comment/:commentId", verifyJWT, createLike);
 
 // Like a video
-router.post("/like/video/:videoId", verifyJWT, createLike);
+router.post("/video/:videoId", verifyJWT, createLike);
 
 // Like a tweet
-router.post("/like/tweet/:tweetId", verifyJWT, createLike);
+router.post("/tweet/:tweetId", verifyJWT, createLike);
 
 // Remove like from comment
-router.delete("/like/comment/:commentId", verifyJWT, removeLike);
+router.delete("/comment/:commentId", verifyJWT, removeLike);
 
 // Remove like from video
-router.delete("/like/video/:videoId", verifyJWT, removeLike);
+router.delete("/video/:videoId", verifyJWT, removeLike);
 
 // Remove like from tweet
-router.delete("/like/tweet/:tweetId", verifyJWT, removeLike);
+router.delete("/tweet/:tweetId", verifyJWT, removeLike);
 
 // Get like count for comment
-router.get("/like/comment/:commentId/count", countLikes);
+router.get("/comment/:commentId/count", countLikes);
 
 // Get like count for video
-router.get("/like/video/:videoId/count", countLikes);
+router.get("/video/:videoId/count", countLikes);
 
 // Get like count for tweet
-router.get("/like/tweet/:tweetId/count", countLikes);
+router.get("/tweet/:tweetId/count", countLikes);
 
 export default router;
