1e32658a708a2e10cc656fe3378c907dc1b22bf5
BhishanSharma <bhishansharma2@gmail.com>
2025-05-16 13:29:27 +0530

    update tweet routes

--- src/routes/tweet.route.js
+++ src/routes/tweet.route.js
@@ -10,15 +10,15 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 // Create a new tweet
-router.route("/tweet").post(verifyJWT, createTweet);
+router.route("/").post(verifyJWT, createTweet);
 
 // Delete a tweet by tweetId (query param)
-router.route("/tweet/:tweetId/delete").delete(verifyJWT, removeTweet);
+router.route("/:tweetId/delete").delete(verifyJWT, removeTweet);
 
 // Edit a tweet by tweetId (query param)
-router.route("/tweet/:tweetId/edit").patch(verifyJWT, editTweet);
+router.route("/:tweetId/edit").patch(verifyJWT, editTweet);
 
 // Get all tweets by logged in user
-router.route("/tweets/my").get(verifyJWT, myTweets);
+router.route("/my").get(verifyJWT, myTweets);
 
 export default router;
