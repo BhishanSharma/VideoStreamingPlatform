eb192f52fab5e4f81a13f77ed35e97732193ca3f
BhishanSharma <bhishansharma2@gmail.com>
2025-05-16 13:30:46 +0530

    update subscription routes

--- src/routes/subscription.route.js
+++ src/routes/subscription.route.js
@@ -9,12 +9,12 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 // Subscribe to a channel (channel id in route param)
-router.route("/subscribe/:channelId").post(verifyJWT, subscribe);
+router.route("/:channelId").post(verifyJWT, subscribe);
 
 // Unsubscribe from a channel (channel id in route param)
-router.route("/unsubscribe/:channelId").delete(verifyJWT, unsubscribe);
+router.route("/:channelId").delete(verifyJWT, unsubscribe);
 
 // Get list of channels the user is subscribed to
-router.route("/subscriptions/my").get(verifyJWT, getMySubscriptions);
+router.route("/my").get(verifyJWT, getMySubscriptions);
 
 export default router;
